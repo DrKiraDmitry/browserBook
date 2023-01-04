5bb13edcabb14542a09778e2c0b038e7b610e6cf
dmitry <stalker2807sky@yandex.ru>
2023-01-04 20:06:10 +0400

    init

---
+++ declaration.d.ts
@@ -0,0 +1,19 @@
+﻿declare module "*.module.css" {
+  const exports: { [exportName: string]: string };
+  export = exports;
+}
+
+declare module "*.module.sass";
+
+declare module "*.module.scss";
+
+declare module "*.mp3" {
+  const src: string;
+  export default src;
+}
+
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
